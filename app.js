ef7177d27eacd277f83ecf6042793b71a89d43f1
Anthony Nandaa <profnandaa@gmail.com>
2017-03-23 15:58:03 +0300

    initial setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const Bot = require('messenger-bot')
+
+let bot = new Bot({
+    token: process.env.PAGE_TOKEN,
+    verify: process.env.VERIFY_TOKEN,
+});
+
+bot.on('error', (err) => {
+    console.log(err.message)
+});
+
+bot.on('message', (payload, reply) => {
+    let text = payload.message.text;
+    console.log(text);
+});
+
+let port = process.env.PORT || 3000;
+http.createServer(bot.middleware()).listen(port);
